b98e5e5c414c5f30c7342cdef30d8ffe9135f500
jjcc <jjccforth@gmail.com>
2018-07-10 21:57:44 -0400

    some documenation

--- generalbot/main.py
+++ generalbot/main.py
@@ -18,4 +18,13 @@ def main():
     #scrapy.cmdline.execute(argv=['scrapy', 'crawl', 'shopclues'])
 
 if  __name__ =='__main__':
+    '''
+    To collect crypto information.
+    1.Run main() to collect crypto information following the list and details, along with those that have
+      normal web site. Output will be "crypto_utf8_allonce.json"
+    2.Complement the collected info with running simple_crawler.py to collect extra info with SPA web
+     (There are some exceptions using Angular contain some static info but others dynamic. Tether is an example)
+      Output will be "amendment_xxx.json"
+    3.Run insert_intdb.py to combine above 2 output and integrate the result into DB 
+    '''
     main()
