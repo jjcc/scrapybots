ee1e1262acb97edf2731dfeca6162654972ea3b8
jjcc <jjccforth@gmail.com>
2018-08-06 21:04:32 -0400

    coinmarketcap API bot

---
+++ independent/coinscrap.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio
+from pymarketcap import AsyncPymarketcap
+
+#count = 0
+
+async def main():
+    count  = 0
+    mylist = ['BTC',"ETH","XRP",'BCH','EOS','XLM','LTC','ADA','USDT','MIOTA']
+    async with AsyncPymarketcap() as apym:
+        async for currency in apym.every_currency(mylist):
+            if count > 100:
+                break
+            print(currency)
+            print(",")
+            count +=1
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    print('[\n')
+    loop.run_until_complete(main())
+    print('\n]')
