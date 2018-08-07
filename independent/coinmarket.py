6d39ed4f8bbe1fc0b601e67a603ce59b41dec2dd
J Chen <jjccforth@gmail.com>
2018-08-07 00:02:50 -0400

    bug fix

--- independent/coinmarket.py
+++ independent/coinmarket.py
@@ -14,7 +14,7 @@ def main():
 
     file = "marketcap.csv"
     with open(file, 'w') as outfile:
-        line = "id,name,symbol,website_slug,rank,circulating,total,max,market"
+        line = "id,name,symbol,website_slug,rank,circulating,total,max,market,last_update\n"
         outfile.write(line)
         for id, info in data.items():
             print("###%s"%info['symbol'])
@@ -22,11 +22,11 @@ def main():
             for key,value in info.items():
                 if key != "quotes":
                     #print("%s:%s"%(key, value))
-                    line = line + "," + str(value)
+                    line = line + str(value) +  ","
                 else:
                     #print("market:%s"%value["USD"]["market_cap"])
-                    line = line + str(value["USD"]["market_cap"])
-                outfile.write(line)
+                    line = line + str(value["USD"]["market_cap"]) + ","
+            outfile.write(line + "\n")
 
     #for c in currencies:
 
