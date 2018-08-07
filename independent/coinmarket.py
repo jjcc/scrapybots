134b9d89a1b43f947dc7c4c74ab57818a587e12b
J Chen <jjccforth@gmail.com>
2018-08-06 23:53:12 -0400

    output coin list

--- independent/coinmarket.py
+++ independent/coinmarket.py
@@ -9,6 +9,28 @@ def main():
 
     # Get all currencies ranked by volume
     currencies = cmc.ticker()
+    num = currencies['metadata']['num_cryptocurrencies']
+    data = currencies['data']
+
+    file = "marketcap.csv"
+    with open(file, 'w') as outfile:
+        line = "id,name,symbol,website_slug,rank,circulating,total,max,market"
+        outfile.write(line)
+        for id, info in data.items():
+            print("###%s"%info['symbol'])
+            line = ""
+            for key,value in info.items():
+                if key != "quotes":
+                    #print("%s:%s"%(key, value))
+                    line = line + "," + str(value)
+                else:
+                    #print("market:%s"%value["USD"]["market_cap"])
+                    line = line + str(value["USD"]["market_cap"])
+                outfile.write(line)
+
+    #for c in currencies:
+
+
     print(currencies)
 
 if __name__ == "__main__":
