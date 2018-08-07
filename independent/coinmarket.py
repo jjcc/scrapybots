4209ae117af075b536abff9747c6cca89fe1e4dc
J Chen <jjccforth@gmail.com>
2018-08-07 00:27:23 -0400

    added pandas funcitons

--- independent/coinmarket.py
+++ independent/coinmarket.py
@@ -1,6 +1,7 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import pandas as pd
+
 
 from pymarketcap import Pymarketcap
 
@@ -13,21 +14,31 @@ def main():
     data = currencies['data']
 
     file = "marketcap.csv"
+    datain = []
     with open(file, 'w') as outfile:
         line = "id,name,symbol,website_slug,rank,circulating,total,max,market,last_update\n"
         outfile.write(line)
         for id, info in data.items():
+            # for pd
+            row = {}
             print("###%s"%info['symbol'])
             line = ""
             for key,value in info.items():
                 if key != "quotes":
                     #print("%s:%s"%(key, value))
                     line = line + str(value) +  ","
+                    row[key] = value
                 else:
                     #print("market:%s"%value["USD"]["market_cap"])
                     line = line + str(value["USD"]["market_cap"]) + ","
+                    row['market_cap'] = value["USD"]["market_cap"]
             outfile.write(line + "\n")
+            datain.append(row)
 
+    df = pd.DataFrame(datain)
+
+    print(df.head())
+    print(df.tail())
     #for c in currencies:
 
 
